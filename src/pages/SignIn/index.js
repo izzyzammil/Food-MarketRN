b6f4ee246fe13fb26da82330d4fb303dfa07c4a5
unknown <izzy.zammil@gmail.com>
2022-03-15 15:59:22 +0700

    Manage Submit Form Using Local State

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,15 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Gap, Header, TextInput} from '../../components';
 
 const SignIn = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = () => {
+    console.log('email', email);
+    console.log('password', password);
+  };
   return (
     <View style={styles.page}>
       <Header title={'Sign In'} subTitle={'Find your best ever meal'} />
@@ -10,11 +17,19 @@ const SignIn = ({navigation}) => {
         <TextInput
           label={'Email Address'}
           placeholder={'Type your email address'}
+          value={email}
+          onChangeText={value => setEmail(value)}
         />
         <Gap height={16} />
-        <TextInput label={'Password'} placeholder={'Type your password'} />
+        <TextInput
+          label={'Password'}
+          placeholder={'Type your password'}
+          value={password}
+          onChangeText={value => setPassword(value)}
+          secureTextEntry
+        />
         <Gap height={24} />
-        <Button text={'Sign In'} />
+        <Button text={'Sign In'} onPress={onSubmit} />
         <Gap height={12} />
         <Button
           text={'Create New Account'}
